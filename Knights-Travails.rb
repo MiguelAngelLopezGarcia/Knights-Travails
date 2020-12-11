ebcb5d9f00ce4b08d3244c7f0c06d30f6824a45a
Miguel Angel Lopez <malg1992@gmail.com>
2020-12-11 12:51:57 +0100

    Sorted loop problem and added euclidean distance

--- Knights-Travails.rb
+++ Knights-Travails.rb
@@ -1,20 +1,20 @@
-#Genera algún tipo de bucle infinito
-
 class Node
-    attr_accessor :value, :children, :parent
+    attr_accessor :value, :children, :parent, :euclidean_distance
     def initialize(value=nil)
         @value = value
         @children = []
         @parent = nil
+        @euclidean_distance = nil
     end
 end
 
 class Knight_travails
-    attr_accessor :start_position, :end_position, :shorter_path
+    attr_accessor :start_position, :end_position, :shorter_path, :queue_of_nodes
     def initialize(array)
         @start_position = Node.new(array[0])
         @end_position = array[1]
         @shorter_path = []
+        @queue_of_nodes = []
     end
 
     def generate_tree_of_knight_movements (node=@start_position, movements=0)
@@ -23,31 +23,39 @@ class Knight_travails
         if node.value == @end_position
             refresh_shorter_path(node)
         elsif @shorter_path.length == 0 || movements < @shorter_path.length
-            node.children = node.children + [Node.new([node.value[0]+2, node.value[1]+1])] if node.value[0]+2 <= 8 && node.value[1]+1 <= 8 && find_previous_move?(node, [node.value[0]+2, node.value[1]+1]) == false
-            node.children = node.children + [Node.new([node.value[0]+2, node.value[1]-1])] if node.value[0]+2 <= 8 && node.value[1]-1 >= 1 && find_previous_move?(node, [node.value[0]+2, node.value[1]-1]) == false
-            node.children = node.children + [Node.new([node.value[0]-2, node.value[1]+1])] if node.value[0]-2 >= 1 && node.value[1]+1 <= 8 && find_previous_move?(node, [node.value[0]-2, node.value[1]+1]) == false
-            node.children = node.children + [Node.new([node.value[0]-2, node.value[1]-1])] if node.value[0]-2 >= 1 && node.value[1]-1 >= 1 && find_previous_move?(node, [node.value[0]-2, node.value[1]-1]) == false
-            node.children = node.children + [Node.new([node.value[0]+1, node.value[1]+2])] if node.value[0]+1 <= 8 && node.value[1]+2 <= 8 && find_previous_move?(node, [node.value[0]+1, node.value[1]+2]) == false
-            node.children = node.children + [Node.new([node.value[0]+1, node.value[1]-2])] if node.value[0]+1 <= 8 && node.value[1]-2 >= 1 && find_previous_move?(node, [node.value[0]+1, node.value[1]-2]) == false
-            node.children = node.children + [Node.new([node.value[0]-1, node.value[1]+2])] if node.value[0]-1 >= 1 && node.value[1]+2 <= 8 && find_previous_move?(node, [node.value[0]-1, node.value[1]+2]) == false
-            node.children = node.children + [Node.new([node.value[0]-1, node.value[1]-2])] if node.value[0]-1 >= 1 && node.value[1]-2 >= 1 && find_previous_move?(node, [node.value[0]-1, node.value[1]-2]) == false
-            i = 0
-            while i < node.children.length
-                node.children[i].parent = node
-                generate_tree_of_knight_movements(node.children[i], movements)
-                i += 1
+            node.children = node.children + [Node.new([node.value[0]+2, node.value[1]+1])] if node.value[0]+2 <= 8 && node.value[1]+1 <= 8 && is_node_in_tree?([node.value[0]+2, node.value[1]+1]) == false
+            node.children = node.children + [Node.new([node.value[0]+2, node.value[1]-1])] if node.value[0]+2 <= 8 && node.value[1]-1 >= 1 && is_node_in_tree?([node.value[0]+2, node.value[1]-1]) == false
+            node.children = node.children + [Node.new([node.value[0]-2, node.value[1]+1])] if node.value[0]-2 >= 1 && node.value[1]+1 <= 8 && is_node_in_tree?([node.value[0]-2, node.value[1]+1]) == false
+            node.children = node.children + [Node.new([node.value[0]-2, node.value[1]-1])] if node.value[0]-2 >= 1 && node.value[1]-1 >= 1 && is_node_in_tree?([node.value[0]-2, node.value[1]-1]) == false
+            node.children = node.children + [Node.new([node.value[0]+1, node.value[1]+2])] if node.value[0]+1 <= 8 && node.value[1]+2 <= 8 && is_node_in_tree?([node.value[0]+1, node.value[1]+2]) == false
+            node.children = node.children + [Node.new([node.value[0]+1, node.value[1]-2])] if node.value[0]+1 <= 8 && node.value[1]-2 >= 1 && is_node_in_tree?([node.value[0]+1, node.value[1]-2]) == false
+            node.children = node.children + [Node.new([node.value[0]-1, node.value[1]+2])] if node.value[0]-1 >= 1 && node.value[1]+2 <= 8 && is_node_in_tree?([node.value[0]-1, node.value[1]+2]) == false
+            node.children = node.children + [Node.new([node.value[0]-1, node.value[1]-2])] if node.value[0]-1 >= 1 && node.value[1]-2 >= 1 && is_node_in_tree?([node.value[0]-1, node.value[1]-2]) == false
+            
+            node.children.each do |child|
+                child.parent = node
+                child.euclidean_distance = claculate_euclidean_distance(child.value)
+                @queue_of_nodes.push(child)
             end
+
+            @queue_of_nodes.sort_by { |node| node.euclidean_distance}
+            next_node_of_the_tree = @queue_of_nodes.shift()
+            generate_tree_of_knight_movements(next_node_of_the_tree, movements)
         end
     end
 
-    def find_previous_move?(node, value)
-        if node.value == value
-            true
-        elsif node.parent != nil
-            find_previous_move?(node.parent, value)
-        else
-            false
+    def is_node_in_tree?(value)
+        queue = [@start_position]
+        while queue.length > 0
+            if queue[0].value == value
+                return true
+            else
+                children_node = queue[0].children
+                children_node.each { |node| queue.push(node)}
+                queue.shift()
+            end
         end
+        false
     end
 
     def refresh_shorter_path(node)
@@ -58,9 +66,12 @@ class Knight_travails
             refresh_shorter_path(node.parent)
         end
     end
+
+    def claculate_euclidean_distance(array)
+        return Math.sqrt((@end_position[0]-array[0])**2 + (@end_position[1]-array[1])**2)
+    end
 end
 
-prueba = Knight_travails.new([[4, 4], [8, 4]])
-p prueba.start_position
-p prueba.generate_tree_of_knight_movements
+prueba = Knight_travails.new([[7, 1], [2, 7]])
+prueba.generate_tree_of_knight_movements
 p prueba.shorter_path
